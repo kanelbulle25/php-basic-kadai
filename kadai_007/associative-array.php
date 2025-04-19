706688b2f2e2c2301bc44256976438a321041f39
kanelbulle25 <55.rbesuccessful@gmail.com>
2025-04-19 15:05:46 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+ <meta charset="UTF-8">
+ <title>課題7章</title>
+<head>
+
+<body>
+ <p>
+   <?php
+   $vagtable_data = ['name' => 'onion', 'price' => '200', 'weight' => '160'];
+   print_r($vagtable_data);
+   ?>
+ </p>
+</body>
+
+</html>
